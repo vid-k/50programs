a6b33b977a036edc4958a63eaad8ae51db8a6e7d
Vidhya Kasiviswanathan <vidhya.kasiviswanathan@gmail.com>
2016-05-18 12:24:09 -0700

    Initial version

---
+++ src/com/basic/FileWordCount8b.java
@@ -0,0 +1,38 @@
+package com.basic;
+
+
+import java.io.*;
+
+public class FileWordCount8b {
+
+    public static int wordCount(String filename) {
+        int wc = 0;
+
+        try {
+            FileReader in = new FileReader(filename);
+            BufferedReader br = new BufferedReader(in);
+            int counter = 0;
+            String line = br.readLine();
+            while (line != null) {
+                String a[] = line.split(" ");
+                for (counter = 0; counter < a.length; counter++) {
+                    if (a[counter].length() > 0)
+                        wc++;
+                }
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return wc;
+    }
+
+    public static void main(String args[]) {
+        System.out.println("WordCount = " +
+                wordCount("FileWordCount.txt")
+        );
+    }
+}
