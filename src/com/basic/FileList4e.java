34b96db3ca5537ade07d006766bc474f9864970f
Vidhya Kasiviswanathan <vidhya.kasiviswanathan@gmail.com>
2016-05-19 10:47:09 -0700

    Initial version

---
+++ src/com/basic/FileList4e.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+import java.io.File;
+
+public class FileList4e {
+
+    public static void DirReader(String path) {
+        File folder = new File(path);
+        File listOfFiles[] = folder.listFiles();
+        System.out.println(path);
+        for (int i=0;i<listOfFiles.length;i++)  {
+            if (listOfFiles[i].isFile())
+                System.out.println("File: " + listOfFiles[i].getName());
+            else if(listOfFiles[i].isDirectory())
+                System.out.println("Directory: " + listOfFiles[i].getName());
+        }
+    }
+
+    public static void main(String args[]) {
+        DirReader("c:/Program files");
+    }
+}
